20186d9ae6dd7842fa2c6be04bd06cf0e15ca506
Risa Harada <supikateriburu@gmail.com>
2017-05-15 10:35:33 +0900

    :sparkles: Add button push function
    to check the button

---
+++ Assets/scripts/GetVoiceInput.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetVoiceInput : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void ButtonPush() {
+		Debug.Log("Button Push !!");
+	}
+}
